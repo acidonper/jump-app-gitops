aae3f2ff669cd857e3e1b8ab1806b011cef296c3
Asier Cidon <asier.cidon@gmail.com>
2021-01-24 23:31:49 +0100

    Added update charts reference automatically script

---
+++ scripts/update_charts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Update charts reference in all branches
+#
+
+BRANCHES="feature/jump-app-cicd feature/jump-app-pre feature/jump-app-pro feature/jump-app-dev master"
+
+for i in $BRANCHES
+do
+  echo "Updating charts in $i..."
+  sleep 2
+  git checkout $i
+  git submodule update --remote
+  git add ../charts
+  git commit -m "Added new charts reference"
+  git push origin $i
+  echo ""
+done
