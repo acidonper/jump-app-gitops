4be600e18861a5d7aa8fc485e006355a9c7948e4
Asier Cidon <asier.cidon@gmail.com>
2021-12-02 16:26:16 +0100

    Added deleted files

--- scripts/extra/setup_istio.sh
+++ scripts/extra/setup_istio.sh
@@ -7,5 +7,5 @@
 read -p "Are you sure Service Mesh Operators are installed on istio-system namespace (*If ServiceMesh is enabled)? Press enter to continue..."
 
 # Aplying Controlplane and Memberrole objects
-oc apply -f ./examples/istio-controlplane.yaml
-oc apply -f ./examples/istio-memberrole.yaml
+oc apply -f ../files/istio/istio-controlplane.yaml
+oc apply -f ../files/istio/istio-smmr.yaml
