4752d1098c345d8b4a96117af015c5e02cf88e5a
Asier Cidon <asier.cidon@gmail.com>
2022-10-19 22:01:02 +0200

    Included bootstrap through gitops model

---
+++ scripts/setup_argocd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+##
+# Setup Openshift Environment
+##
+
+echo "Setting Up Jump App stuff in Openshift..."
+
+# Install Operators
+echo "Installing ArgoCD operator..."
+helm template ./charts/jump-app-ocp-bootstrap -f ./scripts/files/values-bootstrap-gitops.yaml --debug | oc apply -f -
+sleep 60
+
+# Apply chart template
+echo "Creating ArgoCD Server and Bootstrap the Openshift Cluster"
+helm template ./charts/jump-app-argocd -f ./scripts/files/values-argocd-gitops.yaml --debug --namespace openshift-gitops | oc apply -f -
+
+# Notifications
+read -p "Openshift Cluster bootstrap finished"
